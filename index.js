37ba43738cb0732d3d02dd40a7907a45c97d6f80
fjosufi3 <fjosufi3@gatech.edu>
2021-08-01 01:06:09 -0400

    Hook up EJS view engine | setup mongodb | dynamically populate dramas

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
-const mongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const ejs = require('ejs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
+var username = 'faiz';
+var password = 'twil-mech-mard';
+var hosts = 'iad2-c10-1.mongo.objectrocket.com:54949,iad2-c10-2.mongo.objectrocket.com:54949,iad2-c10-0.mongo.objectrocket.com:54949';
+var database = 'kdrama_records';
+var options = '?replicaSet=4ab47a3c676949cd8063285864cf0e4a';
+var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+const mongo = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongo.connect(err => {
+    if (err) {
+        console.log("Failed to connect to MongoDB server...");
+    }
+    console.log("Connected to MongoDB server...");
+});
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    const collection = mongo.db("kdrama_records").collection("dramas");
+    collection.find({}).toArray(function(err, dramas) {
+        if (err) throw err;
+        const popularDramas = dramas.filter(drama => drama.type === 'popular');
+        const topDramas = dramas.filter(drama => drama.type === 'top');
+        const movies = dramas.filter(drama => drama.type === 'movie');
+        res.render('index', {
+            popularDramas: popularDramas,
+            topDramas: topDramas,
+            movies: movies
+        })
+    });
 });
 
 app.listen(PORT, () => {
